67cb0485df140a1389a903e2678cb59d3b66d397
chenqiangjun <chenqiangjun@mudu.tv>
2020-06-09 00:01:34 +0800

    Aspect 日志

--- src/main/java/cn/chenqiangjun/web/IndexController.java
+++ src/main/java/cn/chenqiangjun/web/IndexController.java
@@ -4,16 +4,17 @@ package cn.chenqiangjun.web;
 import cn.chenqiangjun.handler.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index(){
-        String blog = null;
-        if (blog == null) {
-            throw new NotFoundException("博客不存在");
-        }
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
+        //String blog = null;
+        //if (blog == null) {
+        //    throw new NotFoundException("博客不存在");
+        //}
         return "index";
     }
 }
